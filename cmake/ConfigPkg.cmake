079496d3f661507bda7075b693bcd18dfd05d3c0
Charlie WONG <1213charlie@163.com>
2019-06-13 22:29:30 +0800

    build: shadowsocks tui

--- cmake/ConfigPkg.cmake
+++ cmake/ConfigPkg.cmake
@@ -3,7 +3,7 @@
 #set(PKG_NAME)
 #set(PKG_VERSION)
 set(PKG_LICENSE "GNU General Public License v3.0")
-set(PKG_BRIEF_SUMMARY "A lightweight and ultra-fast shadowsocks library with Qt")
+set(PKG_BRIEF_SUMMARY "The ShadowSocks TUI/GUI Client & Server")
 #set(PKG_APP_GUI)
 #set(PKG_MANUAL_DIR)
 #set(PKG_DOXYGEN_SOURCE)
@@ -11,8 +11,8 @@ set(PKG_BRIEF_SUMMARY "A lightweight and ultra-fast shadowsocks library with Qt"
 set(PKG_VENDOR "ShadowSocks")
 set(PKG_LOGO ${CMAKE_SOURCE_DIR}/shadowsocks.png)
 
-set(PKG_REPO "https://github.com/charlie-wong/ShadowSocks-Core")
-set(PKG_BUG_REPORT "https://github.com/charlie-wong/ShadowSocks-Core/issues")
+set(PKG_REPO "https://github.com/charlie-wong/ShadowSocks")
+set(PKG_BUG_REPORT "https://github.com/charlie-wong/ShadowSocks/issues")
 set(PKG_MAINTAINER_EMAIL "Charlie WONG <1213charlie@163.com>")
 
 #set(PKG_DOC_HELP)
